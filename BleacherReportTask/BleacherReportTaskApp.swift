cbbf8883b522cee123e8332e0a9a2f4d86fd6ad8
Ahmad Almasri <safeer@Safeers-MacBook-Pro.local>
2023-01-28 10:55:03 +0300

    init

---
+++ BleacherReportTask/BleacherReportTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleacherReportTaskApp.swift
+//  BleacherReportTask
+//
+//  Created by Ahmad Almasri on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BleacherReportTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
